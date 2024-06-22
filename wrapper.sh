7c2494014060ce76578e3efce30be2398dc27ab9
Alex Idowu <alexidowu25@gmail.com>
2024-06-22 02:10:43 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # unset PGHOST to force psql to use Unix socket path
-# this is specific to Railway and allows
+# this is specific to PipeOps and allows
 # us to use PGHOST after the init
 unset PGHOST
 
-## unset PGPORT also specific to Railway
+## unset PGPORT also specific to PipeOps
 ## since postgres checks for validity of
 ## the value in PGPORT we unset it in case
 ## it ends up being empty
